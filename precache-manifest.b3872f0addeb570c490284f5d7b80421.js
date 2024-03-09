f5d1598b19862d41f59cb1fb95722c4b6a8c762c
Fabro2701 <fa_2701@hotmail.com>
2024-03-09 13:32:01 -0500

    Updates

---
+++ precache-manifest.b3872f0addeb570c490284f5d7b80421.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef358917910e4e227ff326de5e28c2ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d50894f628662ced0ca",
+    "url": "/static/css/2.71fe5322.chunk.css"
+  },
+  {
+    "revision": "30b730dd9535099e0358",
+    "url": "/static/css/main.49c8a621.chunk.css"
+  },
+  {
+    "revision": "5d50894f628662ced0ca",
+    "url": "/static/js/2.50ca2875.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.50ca2875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b730dd9535099e0358",
+    "url": "/static/js/main.9229e685.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
